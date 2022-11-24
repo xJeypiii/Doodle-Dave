b56faf61c9b756b8ecbcf45181b42645a067a32b
LAPTOP-E12SGQVN\DAVE <jdavefrancoph@gmail.com>
2022-11-24 11:19:36 +0800

    new animation

---
+++ Assets/Scripts/DeathAnim.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DeathAnim : MonoBehaviour
+{
+    [SerializeField]
+    GameObject deathAnimationObject;
+
+    void OnDestroy()
+    {
+        Instantiate(deathAnimationObject);
+        deathAnimationObject.transform.localPosition = transform.position;
+    }
+
+}
